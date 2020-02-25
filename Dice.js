3533a33a321015afd08aba7bdd9304dbc2d03332
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-02-25 09:00:39 -0800

    init

---
+++ Dice.js
@@ -0,0 +1,18 @@
+let playerOne;
+let playerTwo;
+let playerThree;
+let playerFour;
+let playerFive;
+let playerSix;
+let playerSeven;
+let playerEight;
+let playerNine;
+let playerTen;
+
+//Assign a a number to each for a dice roll for the first function, then write more functions
+//with if statements that ignor the variables that contain 25 or something. Assign the losers'
+//values to 25. 
+//After each round, reset every player's value to 0? Display all, then after button is pressed to roll,
+//redisplay the screen with whatever values have been assigned?
+
+//function to 
