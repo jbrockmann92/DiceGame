eaff1782ef0770e7af2a8001f58eb439601dcfc2
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-02-25 11:12:58 -0800

    Changed playGame function to check against current round and display that the game is over after round 6

--- Dice.js
+++ Dice.js
@@ -14,13 +14,13 @@ let playerTen;
 let playerArray = [playerOne, playerTwo, playerThree, playerFour, playerFive, playerSix, playerSeven, playerEight, playerNine, playerTen];
 
 //Assign a a number to each for a dice roll for the first function, then write more functions
-//with if statements that ignore the variables that contain 25 or something. Assign the losers'
-//values to 25. 
+//with if statements that ignore the variables that contain -1 or something. Assign the losers'
+//values to -1. 
 //After each round, reset every player's value to 0? Display all, then after button is pressed to roll,
 //redisplay the screen with whatever values have been assigned?
 //Each function should have a for loop that loops over how many items are in the list of active players
 //and displays them on the screen
-//Assign the players that aren't at 25 to a list after each round I think
+//Assign the players that aren't at -1 to a list after each round I think
 
 //I think I can have the same function for each round. Just have it ignore any players value is 25
 //Have to have a counter. If less than 4, eliminate two losers, if 4-5, eliminate one
@@ -29,42 +29,52 @@ let playerArray = [playerOne, playerTwo, playerThree, playerFour, playerFive, pl
 //Separate function for the final round? I think so. If statement that tests how many players are left,
 //Then run it if only two
 
-//Function to run the game. For each player whose value isn't 25, print them to screen with button
+//Function to run the game. For each player whose value isn't -1, print them to screen with button
 //to roll their dice, then display the sum of their roles. Maybe display each roll and total?
 
 
 function playGame(array){
-
+    //Probably something with a button to start a new game here
     let counter = 1;
     //Test here against rounds, and call finalRound if necessary. Pass in array
     if (counter === 6){
         finalRound(array);
+        counter++;
     }
     else{
-        fullRound(array);
-        counter++;
+        if (counter < 7){
+            fullRound(array);
+            counter++;
+        }
+        else alert("Game is over!");
     }
 }
 
 function fullRound(array){
-
+    //Game should run six of these
     for (let i = 0; i < array.length; i++){
-        if (array[i] === 25){
+        if (array[i] === -1){
             continue;
         }
         else{
-            array[i] = rollDice(array[i])
+            array[i] = rollDice()
         }
     }
+    let playersRemoved = comparePlayers(array);
 }
 
-function rollDice(player){
-    player = ((Math.random() * 20) + 1);
-
+function rollDice(){
+    let player = (((Math.random() * 4) + 1) + ((Math.random() * 6) + 1) + ((Math.random() * 8) + 1) + ((Math.random() * 10) + 1) + ((Math.random() * 12) + 1) + ((Math.random() * 20) + 1));
+    return player;
 }
 
 function finalRound(array){
 
 }
 
-//Need a function that will compare all numbers returned and assign the losers the int 25
+//Need a function that will compare all numbers returned and assign the losers the int -1
+function comparePlayers(players){
+    players.sort(function(a, b) {return a-b});
+
+    //This doesn't work because it will always take the lowest two, even after those two have 'been removed'
+    }
