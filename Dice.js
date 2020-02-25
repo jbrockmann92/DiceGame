9ec86545998d0b6a28562a0602d55e5dc1eadb02
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-02-25 09:38:59 -0800

    Wrote a number of functions that will be used while playing the game

--- Dice.js
+++ Dice.js
@@ -1,3 +1,4 @@
+"use strict"
 let playerOne;
 let playerTwo;
 let playerThree;
@@ -8,6 +9,9 @@ let playerSeven;
 let playerEight;
 let playerNine;
 let playerTen;
+
+//Should allow pure functions because I can edit what I pass in
+let playerArray = [playerOne, playerTwo, playerThree, playerFour, playerFive, playerSix, playerSeven, playerEight, playerNine, playerTen];
 
 //Assign a a number to each for a dice roll for the first function, then write more functions
 //with if statements that ignore the variables that contain 25 or something. Assign the losers'
@@ -27,3 +31,34 @@ let playerTen;
 
 //Function to run the game. For each player whose value isn't 25, print them to screen with button
 //to roll their dice, then display the sum of their roles. Maybe display each roll and total?
+
+
+function playGame(array){
+
+    let counter = 1;
+    //Test here against rounds, and call finalRound if necessary. Pass in array
+    if (counter === 6){
+        finalRound(array);
+    }
+}
+
+function fullRound(array){
+
+    for (let i = 0; i < 6; i++){
+        if (array[i] === 25){
+            continue;
+        }
+        else{
+            rollDice(array[i])
+        }
+    }
+}
+
+function rollDice(player){
+
+
+}
+
+function finalRound(array){
+
+}
